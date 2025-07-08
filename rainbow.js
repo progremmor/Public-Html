3048f91fbad5c6f582c715580dd4e4c333704be4
progremmor <rzhang9@wpi.edu>
2025-07-08 16:02:50 -0400

    javascript time

--- rainbow.js
+++ rainbow.js
@@ -1,2 +1,42 @@
-let element=document.getElementById("twscontainer");
+let element=document.getElementById("tswcontainer");
 console.log(element);
+let angle=0;
+let tune=document.getElementById("spookytuney")
+let video=document.getElementById("cookedblender")
+let input=document.getElementById("colorInput")
+let increment=0.3;
+let shouldRainbow=true;
+
+tune.loop=true;
+video.loop=true;
+
+function gradOnFrame(){
+    requestAnimationFrame(gradOnFrame);
+    if(shouldRainbow){
+        video.autoplay=true;
+        tune.autoplay=true;
+        element.style.backgroundColor="hsl("+angle+"deg, 100%, 50%)"
+    }else{
+        tune.autoplay=false;   
+        video.autoplay=false; 
+    }
+    if(angle<360){
+        angle+=increment;
+    }else{
+        angle=increment;
+    }
+}
+gradOnFrame();
+function startRainbow(){
+    shouldRainbow=true;
+
+}
+function onM1(event){
+    shouldRainbow=false
+    if(input.value!=""){
+        element.style.backgroundColor=input.value;
+    }else{
+        setTimeout(startRainbow, 1000);
+    }
+    console.log(video);
+}
